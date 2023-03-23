75c1658695808be27b8574bb532f8c0696908572
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-03-23 21:40:37 +0700

    Initial Commit

---
+++ Layout-Practise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Layout-Practise
+//
+//  Created by Елена on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
