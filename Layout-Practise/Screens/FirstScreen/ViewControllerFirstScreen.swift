cc4a3b863434a353f2b25138f60ddf508782c08f
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-03-24 22:37:07 +0700

    Добавила отступы по горизонтали для text field

--- Layout-Practise/Screens/FirstScreen/ViewControllerFirstScreen.swift
+++ Layout-Practise/Screens/FirstScreen/ViewControllerFirstScreen.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewControllerFirstScreen: UIViewController {
 
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var nameInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.nameInput.addPaddingToTextField()
+        self.emailInput.addPaddingToTextField()
+        self.passwordInput.addPaddingToTextField()
+        
+        self.nameInput.addCornerRadius()
     }
-
-
 }
 
+extension UITextField {
+    func addPaddingToTextField() {
+        let paddingView: UIView = UIView.init(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        
+        self.leftView = paddingView
+        self.leftViewMode = .always
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+    
+    func addCornerRadius() {
+        self.layer.cornerRadius = 12
+    }
+}
